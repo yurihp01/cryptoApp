a4096e71cd2541d1b9a7cff0cf5253963fe23c93
yurihp01 <yurihp01@hotmail.com>
2025-07-03 16:23:16 +0100

    Initial Commit

---
+++ cryptoApp/cryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoAppApp.swift
+//  cryptoApp
+//
+//  Created by Yuri on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct cryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
